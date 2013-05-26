9efaf52bdc76ee93471a2b63b2f481d7b006d927
David Salgado <david@digitalronin.com>
2013-05-26 15:23:00 +0200

    add nginx.conf

--- puppet/modules/nginx/manifests/setup.pp
+++ puppet/modules/nginx/manifests/setup.pp
@@ -14,4 +14,21 @@ class nginx::setup {
   file { "/etc/logrotate.d/nginx":
     source => "puppet:///modules/nginx/nginx.logrotate",
   }
+
+  file { [
+      "/opt/nginx/conf/sites-available",
+      "/opt/nginx/conf/sites-enabled"
+    ]:
+    ensure => directory,
+    owner  => "root",
+    group  => "root"
+  }
+
+  file { "/opt/nginx/conf/nginx.conf":
+    source => "puppet:///modules/nginx/nginx.conf",
+    owner  => "root",
+    group  => "root",
+    mode   => 644
+  }
+
 }
