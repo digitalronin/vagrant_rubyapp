42203c72fbed8aef97ca8cb9c9aecb96a7680f5d
David Salgado <david@digitalronin.com>
2013-05-26 15:01:01 +0200

    reorder lines
    make it easier to include/exclude components

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,20 +1,23 @@
-import "utils/*.pp"
-import "admin"
-import "appuser"
-import "ruby193"
-import "passenger"
-import "nginx"
-import "mongodb"
-
 # Fix for "Could not find group puppet" error.
 group { "puppet": ensure => "present" }
 
 # Determine if this box is a vagrant development VM
 $vagrant_dev_vm = inline_template("<%= FileTest.exists?('/tmp/vagrant-puppet') ? 'true' : 'false' %>")
 
-include appuser
+import "utils/*.pp"
+import "admin"
 include admin::build_dependencies
+
+import "appuser"
+include appuser
+
+import "ruby193"
 include ruby193::build
+
+import "passenger"
 include passenger::install
+import "nginx"
 include nginx::setup
+
+import "mongodb"
 include mongodb::install
